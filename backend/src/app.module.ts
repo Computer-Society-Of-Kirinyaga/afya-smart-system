5d845e57b0470c6a21541797f5d0d349d970d5d7
Murchoid <kamaumkay@gmail.com>
2026-03-27 00:14:40 +0300

    Cleaned up repo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { HealthReadingsModule } from './health-readings/health-readings.module';
 import { RiskAssessmentsModule } from './risk_assessments/risk_assessments.module';
+import { DatabaseModule } from './config/database.module';
 
 @Module({
-  imports: [UsersModule, HealthReadingsModule, RiskAssessmentsModule],
+  imports: [DatabaseModule, UsersModule, HealthReadingsModule, RiskAssessmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
